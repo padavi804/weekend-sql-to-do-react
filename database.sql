a73fdafca2358b1eddde85f6592fe28e514cebcb
Patrick Davison <davisonpatrickj@gmail.com>
2024-05-24 12:12:37 -0500

    imported database and content to db.sql file

---
+++ database.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "todo" (
+"id" serial primary key,
+"note" varchar (250),
+"complete" boolean
+);
+
+INSERT INTO "todo"
+	("note", "complete")
+VALUES
+	('make a database', 'true'),
+	('make a table in the database', 'true'),
+	('write code for the server', 'false');
--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET
-
+router.get('/', (req, res) => {
+    
+})
 // POST
 
 // PUT
