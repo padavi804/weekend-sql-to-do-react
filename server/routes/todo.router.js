af1fc9f48725d6ece42268fdb0cd1290d6b92ea2
Patrick Davison <davisonpatrickj@gmail.com>
2024-05-27 09:06:06 -0500

    added POST route

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -36,7 +36,35 @@ VALUES	($1, $2);`
         })
 });
 // PUT
+router.put('/:id', (req, res) => {
+    console.log(req.params);
+    console.log(req.body);
+    let idToupdate = req.params.id;
+    let complete = req.body.complete;
 
+    let queryText;
+    if(complete === true) {
+        complete = false;
+        queryText = 'UPDATE "todo" SET transfer=false WHERE id=$1;';
+    }else if(complete === false) {
+        queryText = 'UPDATE "todo" SET transfer=true WHERE id=$1;';
+        complete = true;
+        res.sendStatus(400);
+    }
+
+    console.log('transfer after if:', complete);
+
+    pool.query(queryText, [idToupdate])
+    .then(dbResult => {
+        console.log(dbResult);
+        res.sendStatus(200);
+        
+    })
+    .catch(dbError => {
+        console.log(dbError);
+        res.sendStatus(500);
+    })
+});
 // DELETE
 router.delete('/:id', (req, res) => {
 
