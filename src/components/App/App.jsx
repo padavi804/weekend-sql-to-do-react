8af4002d3a305ce1d279d3f1cef50f32d5bd00e8
Patrick Davison <davisonpatrickj@gmail.com>
2024-06-02 21:37:43 -0500

    begin on add.jsx. added form

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,22 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
-function App () {
-  
+function App() {
+
   return (
     <div>
       <h1>TO DO APP</h1>
+
+
+      <section className="new-task">
+        <form>
+        <input id="name-input" placeholder="New Task"/>
+        <input type="checkbox" id="Complete" />
+        <button type="submit">Add to list</button>
+        </form>
+      </section>
     </div>
   );
+
 
 }
 
